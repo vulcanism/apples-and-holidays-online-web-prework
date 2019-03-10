45ef5d6fc4570fb5823aa42bd09803a8cfd6a410
Savannah C <sgcorrell@gmail.com>
2019-03-10 16:41:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,6 +83,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
+  array = []
+  
   holiday_hash
 
 end
